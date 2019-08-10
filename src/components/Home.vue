a7dbce259f93159afbfcd52ef9f51faab95c5ba9
Temitope <temmieayodele@gmail.com>
2019-08-10 19:43:14 +0100

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <Header/>
+  </div>
+</template>
+
+<script>
+import Header from "./Header.vue";
+export default {
+  name: "Home",
+  components: {Header},
+  // props: {
+  //   msg: String
+  // }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
