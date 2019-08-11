83ba319b8eccce40e4f04d8775369583b997d473
Temitope <temmieayodele@gmail.com>
2019-08-11 20:39:15 +0100

    api call

--- src/components/Starships.vue
+++ src/components/Starships.vue
@@ -2,47 +2,40 @@
     <div class="starships">
         <h1>Popular Starships</h1>
         <hr/>
-        <div>
-  <!-- <b-card
-    title="Card Title"
-    img-src="https://picsum.photos/600/300/?image=25"
-    img-alt="Image"
-    img-top
-    tag="article"
-    style="max-width: 20rem;"
-    class="mb-2"
-  >
-    <b-card-text>
-      Some quick example text to build on the card title and make up the bulk of the card's content.
-    </b-card-text>
-
-    <b-button href="#" variant="primary">Go somewhere</b-button>
-  </b-card> -->
-</div>
+        <!-- <div v-for="(starship, index) in starships" :key="index>
+            
+        </div> -->
+        
     </div>
 </template>
 
 <script>
 export default {
-    name: "Starships"
-}
+  name: "Starships",
+  props: {
+    starships: Array
+  }
+
+};
 </script>
+
 <style scoped>
-.starships{
-    display: flex;
-    flex-flow: column;
-    justify-content: center;
-    align-items: center;
-    padding-top: 50px
+.starships {
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: 50px;
 }
-h1{
-    color: rgb(34, 33, 33);
-    padding-bottom: none;
+h1 {
+  color: rgb(34, 33, 33);
+  margin-bottom: none;
 }
-hr{
-    width: 100px;
-    height: 10px;
-    background-color: rgb(34, 33, 33);
+hr {
+  width: 100px;
+  height: 6px;
+  background-color: rgb(34, 33, 33);
+  padding-top: none;
 }
 </style>
 
