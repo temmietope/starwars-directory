56fd7f13256677ff0c58cc27893d3b416c822836
Temitope <temmieayodele@gmail.com>
2019-08-11 20:39:00 +0100

    first commit

---
+++ src/components/Planets.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="planets">
+        <h1>Popular Planets</h1>
+        <hr/>
+        <div>
+  
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Planets",
+  props: {
+    planets: Array,
+  }
+
+};
+</script>
+<style scoped>
+.planets {
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: 50px;
+}
+h1 {
+  color: rgb(34, 33, 33);
+  margin-bottom: none;
+}
+hr {
+  width: 100px;
+  height: 6px;
+  background-color: rgb(34, 33, 33);
+  padding-top: none;
+}
+</style>
+
+
+
