19e080e05e41a8f6fa75a075c657b5ef1ddd7fcd
Temitope <temmieayodele@gmail.com>
2019-08-15 17:35:11 +0100

    added more props and a new function

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -5,6 +5,9 @@
             <img alt="moreDetails.name" :src="moreDetails.imgUrl"/>
             <span>{{moreDetails.name}}</span>
         </div>
+        <div><button @click="load()">prev</button>
+        <button>next</button>
+        </div>
         <div class="body"><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates maxime libero alias accusamus tempora explicabo laudantium, distinctio nihil, dolorum quia unde eveniet accusantium non nulla corporis nobis perspiciatis est sint quibusdam quos blanditiis maiores praesentium? Quo, omnis corrupti soluta veniam deleniti praesentium ullam aperiam sequi nulla ut tenetur natus hic.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit cumque aperiam quia! At molestiae adipisci ducimus suscipit, nam minus ipsa a officia sunt quaerat id veritatis itaque amet quasi. Numquam cumque deserunt dignissimos soluta ad neque eaque maxime earum quasi. Aliquid at dolorum tenetur! Similique magni quasi enim hic ex voluptatibus quod cumque laborum illo repudiandae deserunt corporis, fugiat provident modi doloribus optio delectus reprehenderit blanditiis cum, temporibus voluptas est! Qui consequuntur, tenetur laboriosam quo temporibus voluptates aperiam et explicabo.</p>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates maxime libero alias accusamus tempora explicabo laudantium, distinctio nihil, dolorum quia unde eveniet accusantium non nulla corporis nobis perspiciatis est sint quibusdam quos blanditiis maiores praesentium? Quo, omnis corrupti soluta veniam deleniti praesentium ullam aperiam sequi nulla ut tenetur natus hic.</p>
@@ -17,7 +20,13 @@ export default {
   name: "Details",
   props: {
     moreDetails: Object,
-    arrayList: Array
+    arrayList: Array,
+    index: Number
+  },
+  methods: {
+    load() {
+      console.log(this.index);
+    }
   },
   mounted() {
     console.log(this.moreDetails, this.arrayList);
