25bc9f48f4659696a0fa84dffb122f2985baca6b
Temitope <temmieayodele@gmail.com>
2019-08-19 17:22:44 +0100

    worked on the ui

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -7,7 +7,32 @@
         <div><button @click="previous()" :disabled="newIndex===0">prev</button>
         <button @click="next()" :disabled="newIndex===arrayList.length-1">next</button>
         </div>
-        <div class="body"><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates maxime libero alias accusamus tempora explicabo laudantium, distinctio nihil, dolorum quia unde eveniet accusantium non nulla corporis nobis perspiciatis est sint quibusdam quos blanditiis maiores praesentium? Quo, omnis corrupti soluta veniam deleniti praesentium ullam aperiam sequi nulla ut tenetur natus hic.</p>
+        <div class="body">
+          <h3>About</h3>
+          <p>Name: {{newObject.name}}</p>
+
+          <p v-if="newObject.birth_year">Birth-year: {{newObject.birth_year}}</p>
+          <p v-if="newObject.gender==='n/a'">Robot</p>
+          <p v-if="newObject.gender">Gender: {{newObject.gender}}</p>
+          <p v-if="newObject.eye_color">Eye-color: {{newObject.eye_color}}</p>
+          <p v-if="newObject.skin_color">Skin-color: {{newObject.skin_color}}</p>
+          <p v-if="newObject.height">Height: {{newObject.height}}</p>
+          <p v-if="newObject.mass">Mass: {{newObject.mass}}</p>
+
+          <p v-if="newObject.model">Model: {{newObject.model}}</p>
+          <p v-if="newObject.manufacturer">Manufacturer: {{newObject.manufacturer}}</p>
+          <p v-if="newObject.consumables">Consumables: {{newObject.consumables}}</p>
+          <p v-if="newObject.hyperdrive_rating">Hyperdrive-rating: {{newObject.hyperdrive_rating}}</p>
+          <p v-if="newObject.starship_class">Class: {{newObject.starship_class}}</p>
+          <p v-if="newObject.cargo_capacity">Cargo-capacity: {{newObject.cargo_capacity}}</p>
+
+          <p v-if="newObject.climate">Temperature: {{newObject.climate}}</p>
+          <p v-if="newObject.gravity">Gravity: {{newObject.gravity}}</p>
+          <p v-if="newObject.population">Population: {{newObject.population}}</p>
+          <p v-if="newObject.rotation_period">Rotation-period: {{newObject.rotation_period}}</p>
+          <p v-if="newObject.orbital_period">Rotation-period: {{newObject.orbital_period}}</p>
+          <p v-if="newObject.terrain">Terrain: {{newObject.terrain}}</p>
+
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit cumque aperiam quia! At molestiae adipisci ducimus suscipit, nam minus ipsa a officia sunt quaerat id veritatis itaque amet quasi. Numquam cumque deserunt dignissimos soluta ad neque eaque maxime earum quasi. Aliquid at dolorum tenetur! Similique magni quasi enim hic ex voluptatibus quod cumque laborum illo repudiandae deserunt corporis, fugiat provident modi doloribus optio delectus reprehenderit blanditiis cum, temporibus voluptas est! Qui consequuntur, tenetur laboriosam quo temporibus voluptates aperiam et explicabo.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit cumque aperiam quia! At molestiae adipisci ducimus suscipit, nam minus ipsa a officia sunt quaerat id veritatis itaque amet quasi. Numquam cumque deserunt dignissimos soluta ad neque eaque maxime earum quasi. Aliquid at dolorum tenetur! Similique magni quasi enim hic ex voluptatibus quod cumque laborum illo repudiandae deserunt corporis, fugiat provident modi doloribus optio delectus reprehenderit blanditiis cum, temporibus voluptas est! Qui consequuntur, tenetur laboriosam quo temporibus voluptates aperiam et explicabo.</p></div>
     </div>
@@ -39,6 +64,7 @@ export default {
     }
   },
   mounted() {
+    console.log(this.arrayList);
     this.newObject = this.moreDetails;
   }
 };
