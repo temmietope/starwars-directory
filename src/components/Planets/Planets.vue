2d4c55a5481b673d0cfc32931b4121b1d7eca766
Temitope <temmieayodele@gmail.com>
2019-08-22 16:01:31 +0100

    added some new functions and also worked on the ui

--- src/components/Planets/Planets.vue
+++ src/components/Planets/Planets.vue
@@ -3,16 +3,19 @@
         <h1>Popular Planets</h1>
         <hr/>
           <div v-if="planets.length=== 0">No Starwars Planet matches your search</div>
-        <div v-else class="flex-container">
-            <div class="planet" v-for="(planet, index) in planetsA" :key="index" >
+        <div v-else>
+          <div class="flex-container">
+            <div class="planet" v-for="(planet, index) in planetsArray" :key="index" >
               <img alt="planet-img" :src="planet.imgUrl"/>
               <router-link :to="{ name: 'Details', params: {moreDetails: planet ,arrayList:planets, index: index}}"><span>{{planet.name}}</span></router-link>
             </div>  
+          </div>
             <div>
-              <button @click="one">1</button>
-              <button @click="two">2</button>
-              <button @click="three">3</button>
-            </div>     
+              <button @click="one"><i class="far fa-circle"></i></button>
+              <button @click="two"><i class="far fa-circle"></i></button>
+              <button @click="three"><i class="far fa-circle"></i></button>
+            </div>    
+        <div class="router-link"><router-link to="/planets"><button>VIEW ALL</button></router-link></div>
         </div>
     </div>
 </template>
@@ -22,34 +25,42 @@ export default {
   name: "Planets",
   data() {
     return {
-      planetsArray: [],
-      planetsA: []
+      planetsA: this.planets
     };
   },
   props: {
     planets: Array
   },
   methods: {
-    getImgUrl(index) {
-      return require("../../assets/planet-" + index + ".jpg");
-    },
     one() {
-      this.planetsA = this.planetsArray.slice(0, 3);
+      this.planetsArray = this.planets.slice(0, 3);
+      console.log(this.planetsArray);
     },
     two() {
-      this.planetsA = this.planetsArray.slice(3, 6);
+      this.planetsArray = this.planets.slice(3, 6);
     },
     three() {
-      this.planetsA = this.planetsArray.slice(6, 9);
+      this.planetsArray = this.planets.slice(6, 9);
     }
   },
-  mounted() {
-    this.planetsArray = this.planets;
-    this.planetsA = this.planetsArray.slice(0, 3);
-    console.log(this.planetsA);
+  computed: {
+    planetsArray: {
+      get() {
+        return this.planetsA.slice(0, 3);
+      },
+      set(planets) {
+        this.planetsA = planets;
+      }
+    }
+  },
+  watch: {
+    planets(newPlanets) {
+      this.planetsA = newPlanets;
+    }
   }
 };
 </script>
+
 <style scoped>
 .planets {
   display: flex;
@@ -92,6 +103,19 @@ img {
   font-size: 20px;
   padding: 5px;
   color: rgb(34, 33, 33);
+}
+.planets button {
+  border: none;
+  background-color: inherit;
+  margin: 10px;
+}
+.planets button i :focus {
+  color: red;
+}
+.router-link button{
+  padding: 7px 150px;
+  border: 1.5px solid rgba(29, 29, 151, 0.863);
+  background-color: azure;
 }
 </style>
 
